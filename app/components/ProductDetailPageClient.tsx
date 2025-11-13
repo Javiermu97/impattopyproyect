9c22efa789c6d4c4ccac7ae28dded3e6eb2d4026
Javier Muñoz <javiermu97@gmail.com>
2025-11-13 10:49:13 -0300

    Primer commit

--- app/components/ProductDetailPageClient.tsx
+++ app/components/ProductDetailPageClient.tsx
@@ -256,7 +256,7 @@ export default function ProductDetailPageClient({ product, relatedProducts }: { 
       icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>,
       content: (
         <>
-          <p>La garantía del producto es de hasta 5 (cinco) días desde la entrega del mismo. Dentro de ese plazo, el usuario puede contactarse con nosotros al +595 123123123 o al correo contacto@arcashop.py para hacer sus reclamos y/o solicitar la devolución de su compra, según lo dicta la Ley del Comercio Electrónico.</p>
+          <p>La garantía del producto es de hasta 5 (cinco) días desde la entrega del mismo. Dentro de ese plazo, el usuario puede contactarse con nosotros al +595 981491155 o al correo impattopy@gmail.com para hacer sus reclamos y/o solicitar la devolución de su compra, según lo dicta la Ley del Comercio Electrónico.</p>
           <p>En el caso de que su pedido haya llegado en malas condiciones, el usuario debe contactarse con nosotros por los mismos medios dentro de los primeros 5 días (Calendario) desde la recepción del producto.</p>
           <p>Nos encargamos de solicitar a PAP el retiro del producto a devolver desde la dirección que nos indique el cliente. El costo de la devolución del producto corre por cuenta del cliente, que debe pagar por el envío a PAP cuando este haga el retiro del mismo.</p>
         </>
